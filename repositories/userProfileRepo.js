aaa5d7f9fc46e996efc2a032e060f42915d15276
optimus9021 <ismaianugrah@gmail.com>
2025-05-12 18:31:19 +0700

    push

--- repositories/userProfileRepo.js
+++ repositories/userProfileRepo.js
@@ -4,7 +4,7 @@ exports.findByFrom = async (from) => {
   return await UserProfile.findOne({ from });
 };
 
-exports.create = async ({ from, name, nik, address }) => {
+exports.create = async ({ from, name, nik, address, jenis_kelamin }) => {
   return await UserProfile.create({ from, name, nik, address, jenis_kelamin });
 };
 
