de72a7eb885627d3507bbbf4e93c499134bf1562
Faiz Elfahad Kurniawan <74404911+siunlucky@users.noreply.github.com>
2025-06-10 13:18:32 +0700

    DEV_Ismail

--- app.js
+++ app.js
@@ -64,10 +64,7 @@ mongoose.connection.on('disconnected', () => {
 });
 
 // Koneksi ke MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log("✅ MongoDB Connected");
 
   // Menjalankan server hanya jika bukan environment test
