74ef828109eeedf51d0aaf51cf23275e030ceef5
kevinirawand <kevinid07@gmail.com>
2025-06-12 05:13:44 +0700

    feature: test limiter

--- middlewares/limitMiddleware.js
+++ middlewares/limitMiddleware.js
@@ -1,5 +1,21 @@
-const limitMiddleware = (req, res, next) => {
 
+const moment = require('moment');
+// Adjust the path below to where your UserSession model is located
+const UserSession = require('../models/UserSession');
+
+const limitMiddleware = async (req, res, next) => {
+    try {
+        const today = moment().startOf('day');
+        const userSessions = await UserSession.find({
+            createdAt: { $gte: today.toDate() }
+        });
+        // Your business logic goes here
+        console.log(userSessions?.length); // For debugging purposes
+        next();
+    } catch (error) {
+        console.error("Error fetching UserSessions:", error);
+        res.status(500).send('Internal Server Error');
+    }
 };
 
 module.exports = limitMiddleware;
--- models/UserSession.js
+++ models/UserSession.js
@@ -31,7 +31,7 @@ const userSessionSchema = new mongoose.Schema({
     type: [mongoose.Schema.Types.ObjectId],
     ref: "Tindakan",
     default: [],
-  }    
+  }
 }, {
   timestamps: true,
 });
